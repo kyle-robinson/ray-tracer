70045ef26147fad6954da620fe40964158043316
Kyle ROBINSON <w021256j@student.staffs.ac.uk>
2022-11-02 10:59:20 +0000

    Cleaned up types.

--- RayTracerSmall/main.cpp
+++ RayTracerSmall/main.cpp
@@ -50,35 +50,38 @@ class Vec3
 {
 public:
 	T x, y, z;
-	Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
-	Vec3(T xx) : x(xx), y(xx), z(xx) {}
-	Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
-	Vec3& normalize()
+	Vec3() : x( T( 0 ) ), y( T( 0 ) ), z( T( 0 ) ) {}
+	Vec3( T xx ) : x( xx ), y( xx ), z( xx ) {}
+	Vec3( T xx, T yy, T zz ) : x( xx ), y( yy ), z( zz ) {}
+	inline Vec3& normalize()
 	{
 		T nor2 = length2();
-		if (nor2 > 0) {
-			T invNor = 1 / sqrt(nor2);
+		if ( nor2 > (T)0 )
+		{
+			T invNor = (T)1 / sqrt( nor2 );
 			x *= invNor, y *= invNor, z *= invNor;
 		}
 		return *this;
 	}
-	Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); }
-	Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
-	T dot(const Vec3<T> &v) const { return x * v.x + y * v.y + z * v.z; }
-	Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
-	Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
-	Vec3<T>& operator += (const Vec3<T> &v) { x += v.x, y += v.y, z += v.z; return *this; }
-	Vec3<T>& operator *= (const Vec3<T> &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
-	Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
-	T length2() const { return x * x + y * y + z * z; }
-	T length() const { return sqrt(length2()); }
-	friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v)
+
+	inline Vec3<T> operator - () const noexcept { return Vec3<T>( -x, -y, -z ); }
+	inline Vec3<T> operator * ( const T &f ) const noexcept { return Vec3<T>( x * f, y * f, z * f ); }
+	inline Vec3<T> operator * ( const Vec3<T> &v ) const noexcept { return Vec3<T>( x * v.x, y * v.y, z * v.z ); }
+	inline Vec3<T> operator - ( const Vec3<T> &v ) const noexcept { return Vec3<T>( x - v.x, y - v.y, z - v.z ); }
+	inline Vec3<T> operator + ( const Vec3<T> &v ) const noexcept { return Vec3<T>( x + v.x, y + v.y, z + v.z ); }
+
+	inline Vec3<T>& operator += ( const Vec3<T> &v ) { x += v.x, y += v.y, z += v.z; return *this; }
+	inline Vec3<T>& operator *= ( const Vec3<T> &v ) { x *= v.x, y *= v.y, z *= v.z; return *this; }
+
+	inline T length() const noexcept { return sqrt( length2() ); }
+	inline T length2() const noexcept { return x * x + y * y + z * z; }
+	inline T dot(const Vec3<T> &v) const noexcept { return x * v.x + y * v.y + z * v.z; }
+	inline friend std::ostream & operator << ( std::ostream &os, const Vec3<T> &v )
 	{
 		os << "[" << v.x << " " << v.y << " " << v.z << "]";
 		return os;
 	}
 };
-
 typedef Vec3<float> Vec3f;
 
 class Sphere
@@ -92,24 +95,31 @@ public:
 		const Vec3f &c,
 		const float &r,
 		const Vec3f &sc,
-		const float &refl = 0,
-		const float &transp = 0,
-		const Vec3f &ec = 0) :
-		center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec),
-		transparency(transp), reflection(refl)
-	{ /* empty */
-	}
+		const float &refl = 0.0f,
+		const float &transp = 0.0f,
+		const Vec3f &ec = 0 )
+		:
+		center( c ),
+		radius( r ),
+		radius2( r * r ),
+		surfaceColor( sc ),
+		emissionColor( ec ),
+		transparency( transp ),
+		reflection( refl )
+	{}
 	//[comment]
 	// Compute a ray-sphere intersection using the geometric solution
 	//[/comment]
-	bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const
+	inline bool intersect( const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1 ) const noexcept
 	{
 		Vec3f l = center - rayorig;
-		float tca = l.dot(raydir);
-		if (tca < 0) return false;
-		float d2 = l.dot(l) - tca * tca;
-		if (d2 > radius2) return false;
-		float thc = sqrt(radius2 - d2);
+		float tca = l.dot( raydir );
+		if ( tca < 0.0f ) return false;
+
+		float d2 = l.dot( l ) - tca * tca;
+		if ( d2 > radius2 ) return false;
+
+		float thc = sqrt( radius2 - d2 );
 		t0 = tca - thc;
 		t1 = tca + thc;
 
@@ -122,9 +132,9 @@ public:
 //[/comment]
 #define MAX_RAY_DEPTH 5
 
-float mix(const float &a, const float &b, const float &mix)
+float mix( const float &a, const float &b, const float &mix )
 {
-	return b * mix + a * (1 - mix);
+	return b * mix + a * ( 1.0f - mix );
 }
 
 //[comment]
@@ -141,78 +151,94 @@ Vec3f trace(
 	const Vec3f &rayorig,
 	const Vec3f &raydir,
 	const std::vector<Sphere> &spheres,
-	const int &depth)
+	const uint_fast32_t &depth )
 {
 	//if (raydir.length() != 1) std::cerr << "Error " << raydir << std::endl;
 	float tnear = INFINITY;
 	const Sphere* sphere = NULL;
+
 	// find intersection of this ray with the sphere in the scene
-	for (unsigned i = 0; i < spheres.size(); ++i) {
+	for ( uint_fast32_t i = 0U; i < (uint_fast32_t)spheres.size(); ++i )
+	{
 		float t0 = INFINITY, t1 = INFINITY;
-		if (spheres[i].intersect(rayorig, raydir, t0, t1)) {
-			if (t0 < 0) t0 = t1;
-			if (t0 < tnear) {
+		if ( spheres[i].intersect( rayorig, raydir, t0, t1 ) )
+		{
+			if ( t0 < 0 ) t0 = t1;
+			if ( t0 < tnear )
+			{
 				tnear = t0;
 				sphere = &spheres[i];
 			}
 		}
 	}
+
 	// if there's no intersection return black or background color
-	if (!sphere) return Vec3f(2);
-	Vec3f surfaceColor = 0; // color of the ray/surfaceof the object intersected by the ray
+	if ( !sphere ) return Vec3f( 2.0f );
+	Vec3f surfaceColor = 0.0f; // color of the ray/surfaceof the object intersected by the ray
 	Vec3f phit = rayorig + raydir * tnear; // point of intersection
 	Vec3f nhit = phit - sphere->center; // normal at the intersection point
-	nhit.normalize(); // normalize normal direction
-					  // If the normal and the view direction are not opposite to each other
-					  // reverse the normal direction. That also means we are inside the sphere so set
-					  // the inside bool to true. Finally reverse the sign of IdotN which we want
-					  // positive.
-	float bias = 1e-4; // add some bias to the point from which we will be tracing
+	
+	// normalize normal direction
+	// If the normal and the view direction are not opposite to each other
+	// reverse the normal direction. That also means we are inside the sphere so set
+	// the inside bool to true. Finally reverse the sign of IdotN which we want
+	// positive.
+	nhit.normalize();
+
+	float bias = INFINITY; // add some bias to the point from which we will be tracing
 	bool inside = false;
-	if (raydir.dot(nhit) > 0) nhit = -nhit, inside = true;
-	if ((sphere->transparency > 0 || sphere->reflection > 0) && depth < MAX_RAY_DEPTH) {
-		float facingratio = -raydir.dot(nhit);
+	if ( raydir.dot( nhit ) > 0.0f ) nhit = -nhit, inside = true;
+	if ( ( sphere->transparency > 0.0f || sphere->reflection > 0.0f ) && depth < MAX_RAY_DEPTH )
+	{
+		float facingratio = -raydir.dot( nhit );
 		// change the mix value to tweak the effect
-		float fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
-		// compute reflection direction (not need to normalize because all vectors
-		// are already normalized)
-		Vec3f refldir = raydir - nhit * 2 * raydir.dot(nhit);
+		float fresneleffect = mix( powf( 1.0f - facingratio, 3.0f ), 1.0f, 0.1f );
+		// compute reflection direction (not need to normalize because all vectors are already normalized)
+		Vec3f refldir = raydir - nhit * 2.0f * raydir.dot( nhit );
 		refldir.normalize();
-		Vec3f reflection = trace(phit + nhit * bias, refldir, spheres, depth + 1);
-		Vec3f refraction = 0;
+		Vec3f reflection = trace( phit + nhit * bias, refldir, spheres, depth + 1U );
+		Vec3f refraction = 0.0f;
+
 		// if the sphere is also transparent compute refraction ray (transmission)
-		if (sphere->transparency) {
-			float ior = 1.1, eta = (inside) ? ior : 1 / ior; // are we inside or outside the surface?
-			float cosi = -nhit.dot(raydir);
-			float k = 1 - eta * eta * (1 - cosi * cosi);
-			Vec3f refrdir = raydir * eta + nhit * (eta *  cosi - sqrt(k));
+		if ( sphere->transparency )
+		{
+			float ior = 1.1f, eta = ( inside ) ? ior : 1.0f / ior; // are we inside or outside the surface?
+			float cosi = -nhit.dot( raydir );
+			float k = 1.0f - eta * eta * ( 1.0f - cosi * cosi );
+			Vec3f refrdir = raydir * eta + nhit * ( eta *  cosi - sqrt( k ) );
 			refrdir.normalize();
-			refraction = trace(phit - nhit * bias, refrdir, spheres, depth + 1);
+			refraction = trace( phit - nhit * bias, refrdir, spheres, depth + 1U );
 		}
 		// the result is a mix of reflection and refraction (if the sphere is transparent)
 		surfaceColor = (
 			reflection * fresneleffect +
-			refraction * (1 - fresneleffect) * sphere->transparency) * sphere->surfaceColor;
+			refraction * ( 1.0f - fresneleffect ) * sphere->transparency ) * sphere->surfaceColor;
 	}
-	else {
+	else
+	{
 		// it's a diffuse object, no need to raytrace any further
-		for (unsigned i = 0; i < spheres.size(); ++i) {
-			if (spheres[i].emissionColor.x > 0) {
+		for ( uint_fast32_t i = 0U; i < (uint_fast32_t)spheres.size(); ++i )
+		{
+			if ( spheres[i].emissionColor.x > 0.0f )
+			{
 				// this is a light
-				Vec3f transmission = 1;
+				Vec3f transmission = 1.0f;
 				Vec3f lightDirection = spheres[i].center - phit;
 				lightDirection.normalize();
-				for (unsigned j = 0; j < spheres.size(); ++j) {
-					if (i != j) {
+				for ( uint_fast32_t j = 0U; j < (uint_fast32_t)spheres.size(); ++j )
+				{
+					if ( i != j )
+					{
 						float t0, t1;
-						if (spheres[j].intersect(phit + nhit * bias, lightDirection, t0, t1)) {
-							transmission = 0;
+						if ( spheres[j].intersect( phit + nhit * bias, lightDirection, t0, t1 ) )
+						{
+							transmission = 0.0f;
 							break;
 						}
 					}
 				}
 				surfaceColor += sphere->surfaceColor * transmission *
-					std::max(float(0), nhit.dot(lightDirection)) * spheres[i].emissionColor;
+					std::max( 0.0f, nhit.dot( lightDirection ) ) * spheres[i].emissionColor;
 			}
 		}
 	}
@@ -225,41 +251,45 @@ Vec3f trace(
 // trace it and return a color. If the ray hits a sphere, we return the color of the
 // sphere at the intersection point, else we return the background color.
 //[/comment]
-void render(const std::vector<Sphere> &spheres, int iteration)
+void render( const std::vector<Sphere> &spheres, uint_fast32_t iteration )
 {
-	// quick and dirty
-	unsigned width = 640, height = 480;
 	// Recommended Testing Resolution
-	//unsigned width = 640, height = 480;
+	float width = 640.0f, height = 480.0f;
 
 	// Recommended Production Resolution
-	//unsigned width = 1920, height = 1080;
+	//float width = 1920.0f, height = 1080.0f;
+
 	Vec3f *image = new Vec3f[width * height], *pixel = image;
-	float invWidth = 1 / float(width), invHeight = 1 / float(height);
-	float fov = 30, aspectratio = width / float(height);
-	float angle = tan(M_PI * 0.5 * fov / 180.);
+	float invWidth = 1.0f / float( width ), invHeight = 1.0f / float( height );
+	float fov = 30.0f, aspectratio = float( width ) / float( height );
+	float angle = tanf( M_PI * 0.5f * fov / 180.0f );
+
 	// Trace rays
-	for (unsigned y = 0; y < height; ++y) {
-		for (unsigned x = 0; x < width; ++x, ++pixel) {
-			float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
-			float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
-			Vec3f raydir(xx, yy, -1);
+	for ( float y = 0.0f; y < height; ++y )
+	{
+		for ( float x = 0.0f; x < width; ++x, ++pixel )
+		{
+			float xx = ( 2.0f * ( ( x + 0.5f ) * invWidth ) - 1 ) * angle * aspectratio;
+			float yy = ( 1.0f - 2.0f * ( ( y + 0.5f ) * invHeight ) ) * angle;
+			Vec3f raydir( xx, yy, -1.0f );
 			raydir.normalize();
-			*pixel = trace(Vec3f(0), raydir, spheres, 0);
+			*pixel = trace( Vec3f( 0.0f ), raydir, spheres, 0U );
 		}
 	}
+
 	// Save result to a PPM image (keep these flags if you compile under Windows)
 	std::stringstream ss;
-	ss << "./spheres" << iteration << ".ppm";
+	ss << "./spheres" << std::to_string( iteration ) << ".ppm";
 	std::string tempString = ss.str();
 	char* filename = (char*)tempString.c_str();
 
-	std::ofstream ofs(filename, std::ios::out | std::ios::binary);
+	std::ofstream ofs( filename, std::ios::out | std::ios::binary );
 	ofs << "P6\n" << width << " " << height << "\n255\n";
-	for (unsigned i = 0; i < width * height; ++i) {
-		ofs << (unsigned char)(std::min(float(1), image[i].x) * 255) <<
-			(unsigned char)(std::min(float(1), image[i].y) * 255) <<
-			(unsigned char)(std::min(float(1), image[i].z) * 255);
+	for ( uint_fast32_t i = 0; i < width * height; ++i )
+	{
+		ofs << (uint_fast8_t)( std::min( 1.0f, image[i].x ) * 255 )
+			<< (uint_fast8_t)( std::min( 1.0f, image[i].y ) * 255 )
+			<< (uint_fast8_t)( std::min( 1.0f, image[i].z ) * 255 );
 	}
 	ofs.close();
 	delete[] image;
@@ -270,13 +300,13 @@ void BasicRender()
 	std::vector<Sphere> spheres;
 	// Vector structure for Sphere (position, radius, surface color, reflectivity, transparency, emission color)
 
-	spheres.push_back(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
-	spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(1.00, 0.32, 0.36), 1, 0.5)); // The radius paramter is the value we will change
-	spheres.push_back(Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
-	spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
+	spheres.push_back( Sphere( Vec3f( 0.0f, -10004.0f, -20.0f ), 10000.0f, Vec3f( 0.20f, 0.20f, 0.20f ), 0.0f, 0.0f ) );
+	spheres.push_back( Sphere( Vec3f( 0.0f,  0.0f,     -20.0f ), 4.0f,     Vec3f( 1.00f, 0.32f, 0.36f ), 1.0f, 0.5f ) ); // The radius paramter is the value we will change
+	spheres.push_back( Sphere( Vec3f( 5.0f, -1.0f,     -15.0f ), 2.0f,     Vec3f( 0.90f, 0.76f, 0.46f ), 1.0f, 0.0f ) );
+	spheres.push_back( Sphere( Vec3f( 5.0f,  0.0f,     -25.0f ), 3.0f,     Vec3f( 0.65f, 0.77f, 0.97f ), 1.0f, 0.0f ) );
 	
 	// This creates a file, titled 1.ppm in the current working directory
-	render(spheres, 1);
+	render( spheres, 1U );
 	std::cout << '\n';
 }
 
@@ -285,39 +315,37 @@ void SimpleShrinking()
 	std::vector<Sphere> spheres;
 	// Vector structure for Sphere (position, radius, surface color, reflectivity, transparency, emission color)
 
-	for (int i = 0; i < 4; i++)
+	for ( uint_fast32_t i = 0U; i < 4U; i++ )
 	{
-		if (i == 0)
+		switch ( i )
 		{
-			spheres.push_back(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
-			spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(1.00, 0.32, 0.36), 1, 0.5)); // The radius paramter is the value we will change
-			spheres.push_back(Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
-			spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
-
-		}
-		else if (i == 1)
-		{
-			spheres.push_back(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
-			spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 3, Vec3f(1.00, 0.32, 0.36), 1, 0.5)); // Radius--
-			spheres.push_back(Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
-			spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
-		}
-		else if (i == 2)
-		{
-			spheres.push_back(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
-			spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 2, Vec3f(1.00, 0.32, 0.36), 1, 0.5)); // Radius--
-			spheres.push_back(Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
-			spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
-		}
-		else if (i == 3)
-		{
-			spheres.push_back(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
-			spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 1, Vec3f(1.00, 0.32, 0.36), 1, 0.5)); // Radius--
-			spheres.push_back(Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
-			spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
+		case 0:
+			spheres.push_back( Sphere( Vec3f( 0.0f, -10004.0f, -20.0f ), 10000.0f, Vec3f( 0.20f, 0.20f, 0.20f ), 0.0f, 0.0f ) );
+			spheres.push_back( Sphere( Vec3f( 0.0f,  0.0f,     -20.0f ), 4.0f,     Vec3f( 1.00f, 0.32f, 0.36f ), 1.0f, 0.5f ) ); // The radius paramter is the value we will change
+			spheres.push_back( Sphere( Vec3f( 5.0f, -1.0f,     -15.0f ), 2.0f,     Vec3f( 0.90f, 0.76f, 0.46f ), 1.0f, 0.0f ) );
+			spheres.push_back( Sphere( Vec3f( 5.0f,  0.0f,     -25.0f ), 3.0f,     Vec3f( 0.65f, 0.77f, 0.97f ), 1.0f, 0.0f ) );
+			break;
+		case 1:
+			spheres.push_back( Sphere( Vec3f( 0.0f, -10004.0f, -20.0f ), 10000.0f, Vec3f( 0.20f, 0.20f, 0.20f ), 0.0f, 0.0f ) );
+			spheres.push_back( Sphere( Vec3f( 0.0f,  0.0f,     -20.0f ), 3.0f,     Vec3f( 1.00f, 0.32f, 0.36f ), 1.0f, 0.5f ) ); // Radius--
+			spheres.push_back( Sphere( Vec3f( 5.0f, -1.0f,     -15.0f ), 2.0f,     Vec3f( 0.90f, 0.76f, 0.46f ), 1.0f, 0.0f ) );
+			spheres.push_back( Sphere( Vec3f( 5.0f,  0.0f,     -25.0f ), 3.0f,     Vec3f( 0.65f, 0.77f, 0.97f ), 1.0f, 0.0f ) );
+			break;
+		case 2:
+			spheres.push_back( Sphere( Vec3f( 0.0f, -10004.0f, -20.0f ), 10000.0f, Vec3f( 0.20f, 0.20f, 0.20f ), 0.0f, 0.0f ) );
+			spheres.push_back( Sphere( Vec3f( 0.0f,  0.0f,     -20.0f ), 2.0f,     Vec3f( 1.00f, 0.32f, 0.36f ), 1.0f, 0.5f ) ); // Radius--
+			spheres.push_back( Sphere( Vec3f( 5.0f, -1.0f,     -15.0f ), 2.0f,     Vec3f( 0.90f, 0.76f, 0.46f ), 1.0f, 0.0f ) );
+			spheres.push_back( Sphere( Vec3f( 5.0f,  0.0f,     -25.0f ), 3.0f,     Vec3f( 0.65f, 0.77f, 0.97f ), 1.0f, 0.0f ) );
+			break;
+		case 3:
+			spheres.push_back( Sphere( Vec3f( 0.0f, -10004.0f, -20.0f ), 10000.0f, Vec3f( 0.20f, 0.20f, 0.20f ), 0.0f, 0.0f) );
+			spheres.push_back( Sphere( Vec3f( 0.0f,  0.0f,     -20.0f ), 1.0f,     Vec3f( 1.00f, 0.32f, 0.36f ), 1.0f, 0.5f) ); // Radius--
+			spheres.push_back( Sphere( Vec3f( 5.0f, -1.0f,     -15.0f ), 2.0f,     Vec3f( 0.90f, 0.76f, 0.46f ), 1.0f, 0.0f) );
+			spheres.push_back( Sphere( Vec3f( 5.0f,  0.0f,     -25.0f ), 3.0f,     Vec3f( 0.65f, 0.77f, 0.97f ), 1.0f, 0.0f) );
+			break;
 		}
 
-		render(spheres, i);
+		render( spheres, i );
 		// Dont forget to clear the Vector holding the spheres.
 		spheres.clear();
 	}
@@ -330,14 +358,14 @@ void SmoothScaling()
 	std::vector<Sphere> spheres;
 	// Vector structure for Sphere (position, radius, surface color, reflectivity, transparency, emission color)
 
-	for (float r = 0; r <= 100; r++)
+	for ( uint_fast32_t i = 0U; i <= 100U; i++ )
 	{
-		spheres.push_back(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
-		spheres.push_back(Sphere(Vec3f(0.0, 0, -20), r / 50, Vec3f(1.00, 0.32, 0.36), 1, 0.5)); // Radius++ change here
-		spheres.push_back(Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
-		spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
-		render(spheres, r);
-		std::cout << "Rendered and saved spheres" << r << ".ppm" << '\n';
+		spheres.push_back( Sphere( Vec3f( 0.0f, -10004.0f, -20.0f), 10000.0f,         Vec3f( 0.20f, 0.20f, 0.20f), 0.0f, 0.0f ) );
+		spheres.push_back( Sphere( Vec3f( 0.0f,  0.0f,     -20.0f), (float)i / 50.0f, Vec3f( 1.00f, 0.32f, 0.36f), 1.0f, 0.5f ) ); // Radius++ change here
+		spheres.push_back( Sphere( Vec3f( 5.0f, -1.0f,     -15.0f), 2.0f,             Vec3f( 0.90f, 0.76f, 0.46f), 1.0f, 0.0f ) );
+		spheres.push_back( Sphere( Vec3f( 5.0f,  0.0f,     -25.0f), 3.0f,             Vec3f( 0.65f, 0.77f, 0.97f), 1.0f, 0.0f ) );
+		render( spheres, i );
+		std::cout << "Rendered and saved spheres" << std::to_string( i ) << ".ppm" << '\n';
 		// Dont forget to clear the Vector holding the spheres.
 		spheres.clear();
 	}
@@ -350,10 +378,10 @@ void SmoothScaling()
 // and 1 light (which is also a sphere). Then, once the scene description is complete
 // we render that scene, by calling the render() function.
 //[/comment]
-int main(int argc, char **argv)
+int main( int argc, char **argv )
 {
 	// This sample only allows one choice per program execution. Feel free to improve upon this
-	srand(13);
+	srand( 13 );
 
 	Timer timer;
 	Heap* heapOne = HeapManager::CreateHeap( "HeapOne" );
